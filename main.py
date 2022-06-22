1e12539d2a3383462d34cee59c7431be32a98711
Narun Yadav <n.y.narun@gmail.com>
2022-06-22 16:17:22 +0530

    flask-prom

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
-from flask import Flask, Response
+from flask import Flask
 from datetime import datetime
-import prometheus_client
-from prometheus_client import Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
+from flask_prom import Prometheus
 
 app = Flask(__name__)
-
-graphs = {}
-graphs['t'] = Counter('python_request_operations_total_time_route',
-                      'The total number of processed requests on time route')
+Prometheus(app)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    try:
+        return "Hello World!"
+    except:
+        return "error occurred"
 
 
 @app.route('/time')
 def time():
-    graphs['t'].inc()
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    return current_time
+    try:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        return current_time
+    except:
+        return "error occurred"
 
 
-@app.route("/metrics")
-def requests_count():
-    res = []
-    for k, v in graphs.items():
-        res.append(prometheus_client.generate_latest(v))
-    return Response(res, mimetype="text/plain")
-
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 # main driver function
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run(host='0.0.0.0')
